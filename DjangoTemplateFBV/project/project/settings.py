022d4a6e481dcd2cfc72559c06311d2d868ab152
akaroshaa <akaroshaa@gmail.com>
2021-11-23 00:26:34 +0530

    commented connectivity with postgresql and brought in sqlite

--- DjangoTemplateFBV/project/project/settings.py
+++ DjangoTemplateFBV/project/project/settings.py
@@ -74,26 +74,26 @@ WSGI_APPLICATION = 'project.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
+}
 
 
 # setting up connectivity with PostgreSQL
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'templatedb',
-        'USER' : 'postgres',
-        'PASSWORD' : 'postgres',
-        'host' : 'localhost',
-        'port' : 5432
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql',
+#         'NAME': 'templatedb',
+#         'USER' : 'postgres',
+#         'PASSWORD' : 'postgres',
+#         'host' : 'localhost',
+#         'port' : 5432
+#     }
+# }
 
 
 
